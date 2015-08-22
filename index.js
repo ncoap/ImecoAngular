c12d9b4d3a08c7beda338049ddfa6fa484734796
Nelson Coqchi Apaza <aries_2221@hotmail.com>
2015-08-22 18:31:51 -0500

    modificaicon de contid operatva rango de colores para

--- index.js
+++ index.js
@@ -8,13 +8,9 @@ angular.module('ngLogin', [
         function saveRol(rol) {
             $cookieStore.put("rol", rol);
         }
-        function removeRol() {
-            $cookieStore.remove('rol');
-        }
         return {
             getRol: getRol,
-            saveRol: saveRol,
-            removeRol: removeRol
+            saveRol: saveRol
         }
     })
     .service('loginService', function ($http, $q) {
@@ -37,23 +33,28 @@ angular.module('ngLogin', [
         }
     })
     .controller('loginController', function ($window, $log, $scope, factoryLogin, loginService, $location) {
-        factoryLogin.removeRol();
-        $scope.usuario = {
-            username: '',
-            password: ''
-        };
-        $scope.login = function () {
-            loginService.login({
-                usuario: $scope.usuario
-            }).then(function (data) {
-                if (data.msj == 'OK') {
-                    factoryLogin.saveRol(data.rol);
-                    window.location.href = "admin.html";
-                } else {
-                    alert(data.mensaje);
-                }
-            }).catch(function (err) {
-                alert("Error en el Server");
-            });
+
+        if(factoryLogin.getRol()) {
+            window.location.href = "admin.html";
+        }else{
+            $scope.usuario = {
+                username: '',
+                password: ''
+            };
+
+            $scope.login = function () {
+                loginService.login({
+                    usuario: $scope.usuario
+                }).then(function (data) {
+                    if (data.msj == 'OK') {
+                        factoryLogin.saveRol(data.rol);
+                        window.location.href = "admin.html";
+                    } else {
+                        alert(data.mensaje);
+                    }
+                }).catch(function (err) {
+                    alert("Error en el Server");
+                });
+            }
         }
     });
--- php/mysql/Conexion.php
+++ php/mysql/Conexion.php
@@ -3,16 +3,16 @@
 class Conexion {
 
     //CONEXTION LOCAL
- /*  public function getConexion() {
+   public function getConexion() {
         $pdo = new PDO("mysql:host=localhost;dbname=prevevbl_odiseamysql", "root", "10200092", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES  \"UTF8\"", PDO::ATTR_EMULATE_PREPARES => false));
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
-    }*/
+    }
 
     //CONEXION http://prevencionenlinea.com/index.php
-    public function getConexion() {
+    /*public function getConexion() {
        $pdo = new PDO("mysql:host=localhost;dbname=preve88h_odisea", "preve88h_victor", "F1l3serve", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES  \"UTF8\"", PDO::ATTR_EMULATE_PREPARES => false));
        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
-    }
+    }*/
 }
